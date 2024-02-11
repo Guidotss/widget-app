620b3ecd8580cd07f8cdcb24083748da1689956c
Guidotss <guidoolguin@hotmail.com>
2024-02-11 11:27:53 -0300

    Creando nuevas rutas y nuevos menu items

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,16 +3,24 @@ import 'package:widget_app/presentations/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen()),
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen()
+  ),
   GoRoute(
     path: '/buttons',
     name: ButtonsScreen.name,
     builder: (context, state) => const ButtonsScreen(),
   ),
   GoRoute(
-      path: '/cards',
-      name: CardsScreen.name,
-      builder: (context, state) => const CardsScreen())
+    path: '/cards',
+    name: CardsScreen.name,
+    builder: (context, state) => const CardsScreen()
+  ),
+  
+  GoRoute(
+    path: '/progress',
+    name: ProgressScreen.name,
+    builder: (context, state) => const ProgressScreen(),
+  ),
 ]);
